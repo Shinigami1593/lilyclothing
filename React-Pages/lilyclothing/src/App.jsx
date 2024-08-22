e51667aa47c2cbb388dd6efdf00352a69df6daf9
Adamya <adamsneupane@gmail.com>
2024-08-22 20:13:25 +0545

    updated everything

--- React-Pages/lilyclothing/src/App.jsx
+++ React-Pages/lilyclothing/src/App.jsx
@@ -1,5 +1,6 @@
 // import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Products from './components/Products';
 import Cart from './components/Cart';
@@ -9,6 +10,20 @@ import SignUp from './components/SignUp';
 function App() {
   return (
     <Router>
+      <header>
+        <nav className="navbar">
+          <div className="logo">
+            <img src="images/lilyclothing.png" alt="Lily Clothing Logo" />
+          </div>
+          <ul className="nav-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+            <li><Link to="/signin" className="signin-btn">Sign in</Link></li>
+            <li><Link to="/signup" className="register-btn">Register</Link></li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
         {/* Define the routes for each page */}
         <Route path="/" element={<Homepage />} />
--- React-Pages/lilyclothing/src/components/Homepage.jsx
+++ React-Pages/lilyclothing/src/components/Homepage.jsx
@@ -5,21 +5,6 @@ import './Homepage.css';
 function Homepage() {
   return (
     <div>
-      <header>
-        <nav className="navbar">
-          <div className="logo">
-            <img src="images/lilyclothing.png" alt="Lily Clothing Logo" />
-          </div>
-          <ul className="nav-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/products">Products</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-            <li><Link to="/signin" className="signin-btn">Sign in</Link></li>
-            <li><Link to="/signup" className="register-btn">Register</Link></li>
-          </ul>
-        </nav>
-      </header>
-
       <section className="hero-section">
         <h1>LilyClothing</h1>
         <button className="get-started-btn">
